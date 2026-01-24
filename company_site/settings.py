35baafe40d8dcd0aaad52211024a174360f3aa47
Ravikumar Shantilal Bhatasana <funnyshortvideo593@gmail.com>
2026-01-24 10:47:13 +0530

    Set DEBUG to False and update ALLOWED_HOSTS

--- company_site/settings.py
+++ company_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2_=0zo8#e^9i$dc*e60&o26g$^p*rrg7!gcuse#_vrq0(tv=6i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
