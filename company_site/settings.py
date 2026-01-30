cc9c7839218f226867d97768a2994bb31830a82d
Ravikumar Shantilal Bhatasana <funnyshortvideo593@gmail.com>
2026-01-30 10:37:44 +0530

    Change static files storage backend to CompressedStaticFilesStorage

--- company_site/settings.py
+++ company_site/settings.py
@@ -123,7 +123,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
